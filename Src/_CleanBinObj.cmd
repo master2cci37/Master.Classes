1deb7c7b52bc0defff26902580dbf5f736886d37
Master 2 CCI Tours <jf.pacory@laposte.net>
2017-04-23 17:09:15 +0200

    import initial

---
+++ Src/_CleanBinObj.cmd
@@ -0,0 +1,17 @@
+﻿@echo off
+@REM //------------------------------------------------------------------------------
+@REM 03/08/2009
+@REM //------------------------------------------------------------------------------
+@REM Nettoyage des repertoires bin et obj de la solution  
+@REM //------------------------------------------------------------------------------
+
+
+rd /S /Q MasterClasses\bin
+rd /S /Q MasterClasses\obj
+
+rd /S /Q MasterClassesTests\bin
+rd /S /Q MasterClassesTests\obj
+
+rd /S /Q TestResults
+
+echo on
